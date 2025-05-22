34add33ae5e3411b08958cf821df4b029794e1f8
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-05-22 16:11:19 -0300

    Add files via upload

---
+++ Lista2Ex9/Lista2Ex9/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2Ex9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double alt, peso, r, gen;
+
+            Console.Write("Qual seu genero? (1) Feminino / (2) Masculina: ");
+            gen = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Qual sua altura? ");
+            alt = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Qual seu peso? ");
+            peso = Convert.ToDouble(Console.ReadLine());
+
+            r = peso / (alt * alt);
+
+
+            if (gen == 1)
+
+            {
+                if (r >= 24.00)
+
+                    Console.WriteLine("Mulher:Acima do peso.");
+
+                if (r < 19.00)
+
+                    Console.WriteLine("Mulher:Abaixo do peso.");
+
+                if (r >= 19.00 && r <= 24.00)
+
+                    Console.WriteLine("Mulher:Peso ideal.");
+            }
+
+            if (gen == 2)
+            {
+                if (r >= 25.00)
+
+                    Console.WriteLine("Homem:Acima do peso.");
+
+                if (r < 20.00)
+
+                    Console.WriteLine("Homem:Abaixo do peso.");
+
+                if (r >= 20.00 && r <= 25.00)
+
+                    Console.WriteLine("Homem:Peso ideal.");
+            }
+
+        }
+    }
+}
