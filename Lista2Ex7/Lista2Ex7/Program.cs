1400231c047ac44d9cfa9d33240283912aa4d9c6
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-05-22 16:10:18 -0300

    Add files via upload

---
+++ Lista2Ex7/Lista2Ex7/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2Ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double l1, l2, l3;
+            Console.Write("Digite o tamanho do lado do triângulo: ");
+            l1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite o tamanho do lado do triângulo: ");
+            l2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite o tamanho do lado do triângulo: ");
+            l3 = Convert.ToDouble(Console.ReadLine());
+
+            if (l1 + l2 > l3 && l2 + l3 > l1 && l3 + l1 > l2)
+            {
+
+
+                if (l1 == l2 && l2 == l3)
+
+                    Console.WriteLine("Forma um triângulo Equilatero.");
+
+                if (l1 != l2 && l1 != l3 && l2 != l3)
+
+                    Console.WriteLine("Forma um triângulo Escaleno.");
+
+                if (l1 == l2 && l2 != l3 || l2 == l3 && l3 != l1 || l1 == l3 && l3 != l2)
+
+                    Console.WriteLine("Forma um triângulo Isósceles.");
+            }
+            else
+            {
+                Console.WriteLine("Nâo forma um triângulo.");
+            }
+        }
+    }
+}
